c9b39e5670d0dc90376dc005731a40fd477432e2
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-24 03:14:23 +0530

    Added licence

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # 
 # Copyright (C) 2022 by YadavGulshan@Github, < https://github.com/YadavGulshan >.
 #
-# This file is part of < https://github.com/Yadavgulshan/PharmaServiceBackend > project,
+# This file is part of < https://github.com/Yadavgulshan/diagonalizable > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/YadavGulshan/diagonalizable/blob/master/LICENCE >
 #
