acd5d23ba1096d5b5a718c7c52e7155261c81266
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-24 03:14:59 +0530

    fixed a typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2022 by YadavGulshan@Github, < https://github.com/YadavGulshan >.
 #
 # This file is part of < https://github.com/Yadavgulshan/diagonalizable > project,
-# and is released under the "GNU v3.0 License Agreement".
+# and is released under the "MIT License".
 # Please see < https://github.com/YadavGulshan/diagonalizable/blob/master/LICENCE >
 #
 # All rights reserved.
